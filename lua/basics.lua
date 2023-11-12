63b60a4e707fe678dca3c666666dc265fc49c88c
Ryan Henning <ryan@rhobota.com>
2023-11-12 04:32:37 +0000

    More key maps

--- lua/basics.lua
+++ lua/basics.lua
@@ -3,10 +3,25 @@ vim.g.mapleader = ','
 vim.g.maplocalleader = ','
 
 -- The <Colon> is too hard to type. I'll remap <Space> for this.
-vim.keymap.set({ 'n', 'v' }, '<Space>', ':')
+vim.keymap.set({ 'n', 'v' }, '<Space>', ':', {desc = 'Mapped to <Colon>'})
 
--- <Esc> is too far away. I'll use 'jj'.
-vim.keymap.set('i', 'jj', '<Esc>')
+-- <ESC> is too far away. I'll use 'jj'.
+vim.keymap.set('i', 'jj', '<ESC>', {desc = 'Mapped to <ESC>'})
+
+-- Other ways to save the buffer:
+vim.keymap.set('n', '<C-s>', ':w<CR>', {silent = true, desc = 'Save Buffer'})
+vim.keymap.set('i', '<C-s>', '<ESC>:w<CR>a', {silent = true, desc = 'Save Buffer'})
+vim.keymap.set('n', '<leader>s', ':w<CR>', {silent = true, desc = 'Save Buffer'})
+vim.keymap.set('i', '<leader>s', '<ESC>:w<CR>a', {silent = true, desc = 'Save Buffer'})
+
+-- Other ways to quit vim:
+vim.keymap.set('n', '<C-q>', ':q<CR>', {silent = true, desc = 'Quit Neovim'})
+vim.keymap.set('i', '<C-q>', '<ESC>:q<CR>', {silent = true, desc = 'Quit Neovim'})
+vim.keymap.set('n', '<leader>q', ':q<CR>', {silent = true, desc = 'Quit Neovim'})
+vim.keymap.set('i', '<leader>q', '<ESC>:q<CR>', {silent = true, desc = 'Quit Neovim'})
+
+-- Clear searches when you don't want to see them anymore:
+vim.keymap.set('n', '<leader>/', ':nohlsearch<CR>', {desc = 'Clear Active Search'})
 
 -- Tell nvim our terminal supports 24bit RGB colors.
 -- See: https://github.com/termstandard/colors
