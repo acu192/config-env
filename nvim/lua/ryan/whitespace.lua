9e093e6e1b1073e6790166ba745001ea00f13a64
Ryan Henning <ryan@rhobota.com>
2024-07-01 17:04:55 -0500

    I haven't missed this feature, so let's remove it.
    This is my old feature where I'd have vim remove trailing whitespace
    when I saved the buffer. I never felt comfortable putting it in my new
    neovim config, and turns out I haven't missed it! So I'm just removing
    the idea entirely.

--- nvim/lua/ryan/whitespace.lua
+++ nvim/lua/ryan/whitespace.lua
@@ -7,10 +7,3 @@ vim.cmd(string.format([[match %s %s]], hl, re1))
 
 vim.cmd(string.format([[autocmd InsertEnter * match %s %s]], hl, re1))
 vim.cmd(string.format([[autocmd InsertLeave * match %s %s]], hl, re2))
-
--- TODO autocmd BufWritePre * :call <SID>StripTrailingWhitespaces()
-
--- I'm worried about this whole feature given that neovim plugins use so many different buffers for different things.
--- We'll need to be super careful to only apply these things to the *code* buffers. We'll probably need to whitelist
--- a set of filetypes to be safe.
-
